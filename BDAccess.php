b5990c76bdb3bf525ed6d105c7962620fc54916d
Leonardo Baldo <baldoleonardo01@gmail.com>
2023-12-13 11:49:43 +0100

    Add files
    Add aggiungiTracciaTemplate.html + discografiaTemplate.html + tecweb2023.sql

--- BDAccess.php
+++ BDAccess.php
@@ -28,9 +28,42 @@
                 }
                 $queryResult -> free();
                 return $result;
-            } else {
+            }else{
                 return null;
             }
+        }
+
+        public function getListaAlbum($id) {
+            $query = "SELECT Album.ID,
+                        Album.Titolo,
+                        Copertina,
+                        DataPubblicazione,
+                        SEC_TO_TIME(SUM(TIME_TO_SEC(Traccia.Durata))) as DurataAlbum
+                        FROM Album
+                        JOIN Traccia ON Album.ID = Traccia.DurataAlbum
+                        WHERE Album.ID = $id";
+            $queryResult = mysqli_query($this -> connection, $query) or die("Errore in DBAccess" . mysqli_error($this -> connection));
+            if(mysqli_num_rows($queryResult) != 0){
+                $row = mysqli_fetch_assoc($queryResult);
+                return array($row["ID"],
+                        $row["Titolo"],
+                        $row["Copertina"],
+                        $row["DataPubblicazione"],
+                        $row["DurataAlbum"]);
+            }else{
+                return null;
+            }
+        }
+
+        public function getTracceAlbum($id) {
+            $query = "SELECT Traccia.Titolo,
+                Traccia.Esplicito,
+                Traccia.Durata,
+                DataRadio,
+                URLVideo
+            FROM Traccia
+            JOIN Album ON Traccia.Album = Album.id
+            WHERE Traccia.Album = $id";
         }
 
         public function closeConnection() {
